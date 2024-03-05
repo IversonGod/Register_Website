b825ba04cfda1ad2d76abdb5d8e21849238cd4af
IversonGod <iversonbondoc33@gmail.com>
2024-03-05 18:22:07 +0800

    register website

---
+++ register.js
@@ -0,0 +1,25 @@
+console.log("Iverson Pogi!");
+console.error("Iverson Pogi mo!");
+console.warn("ayaw ko sayo");
+ //variables
+ // string = "Worddss" , "wowoowo" , "123"
+  // number = 123, 434e, 2398.12
+  // boolean = true , false
+  // object = ["apple" , "banana" ], ["1, 2, 3, 4"] , ["apple" , "1"]
+  // typeof
+  //console.log(typeof(["apple", "banana"]))
+
+// Declaration of Variables
+let name = "IversonPogi",
+lastname = "bondoc",
+ age = 10,
+ isProf = true,
+ hobbies = ["watch tv", "play video games"], 
+ run = function () {
+    console.log("Im running.");
+ };
+ console.log(name, age, isProf, hobbie, run);
+console.log(typeof run);
+console.table(hobbies);
+console.log("My name is" + name + " " + lastname);
+console.log(`My name is ${name} ${lastname}`);
